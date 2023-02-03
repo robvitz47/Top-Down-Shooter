bb8111f684a7b8efd25b9d55f51beab7d1be92ef
Robert Vittozzi <102195553+robvitz47@users.noreply.github.com>
2023-02-03 15:39:16 -0500

    added zombie NPCs

--- main.lua
+++ main.lua
@@ -10,7 +10,7 @@ function love.load()
     player.y = love.graphics.getHeight() / 2
     player.speed = 180
 
-    tempRotation = 0
+    zombies = {}
 end
 
 function love.update(dt)
@@ -26,16 +26,37 @@ function love.update(dt)
     if love.keyboard.isDown("s") then
         player.y = player.y + player.speed*dt
     end
-
-    tempRotation = tempRotation + 0.01
 end
 
 function love.draw()
     love.graphics.draw(sprites.background, 0, 0)
     
     love.graphics.draw(sprites.player, player.x, player.y, playerMouseAngle(), nil, nil, sprites.player:getWidth()/2, sprites.player:getHeight()/2)
+    
+    for i,z in ipairs(zombies) do
+        love.graphics.draw(sprites.zombie, z.x, z.y, zombiePlayerAngle(z), nil, nil, sprites.zombie:getWidth()/2, sprites.zombie:getHeight()/2)
+    end
+end
+
+function love.keypressed( key )
+    if key == "space" then
+        spawnZombie()
+    end
 end
 
 function playerMouseAngle()
     return math.atan2(player.y - love.mouse.getY(), player.x - love.mouse.getX()) + math.pi
+end
+
+function zombiePlayerAngle(enemy)
+    return math.atan2(player.y - enemy.y, player.x - enemy.x)
+end
+
+
+function spawnZombie()
+    local zombie = {}
+        zombie.x = math.random(0, love.graphics.getWidth())
+        zombie.y = math.random(0, love.graphics.getHeight())
+        zombie.speed = 100
+        table.insert(zombies, zombie)    
 end
