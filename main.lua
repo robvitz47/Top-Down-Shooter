84e5d1415965a0c3f36128f4de655382b4350f1c
Robert Vittozzi <102195553+robvitz47@users.noreply.github.com>
2023-02-04 07:35:50 -0500

    added enemy functionality

--- main.lua
+++ main.lua
@@ -11,6 +11,11 @@ function love.load()
     player.speed = 180
 
     zombies = {}
+    bullets = {}
+
+    gameState = 2
+    maxTime = 2
+    timer = maxTime
 end
 
 function love.update(dt)
@@ -34,7 +39,52 @@ function love.update(dt)
         if distanceBetween(z.x, z.y, player.x, player.y) < 28 then
             for i,z in ipairs(zombies) do
                 zombies[i] = nil
+                gameState = 1
             end
+        end
+    end
+
+    for i,b in ipairs(bullets) do
+        b.x = b.x + (math.cos( b.direction ) * b.speed * dt)
+        b.y = b.y + (math.cos( b.direction ) * b.speed * dt)
+    end
+
+    for i=#bullets, 1, -1 do
+        local b = bullets[i]
+        if b.x < 0 or b.y < 0 or b.x > love.graphics.getWidth() or b.y > love.graphics.getHeight() then
+            table.remove(bullets, i)
+        end
+    end
+
+    for i,z in ipairs(zombies) do
+        for j,b in ipairs(bullets) do
+            if distanceBetween(z.x, z.y, b.x, b.y) < 20 then
+                z.dead = true
+                b.dead = true
+            end
+        end
+    end
+
+    for i=#zombies,1,-1 do
+        local z = zombies[i]
+        if z.dead == true then
+            table.remove(zombies, i)
+        end
+    end
+
+    for i=#bullets,1,-1 do
+        local b = bullets[i]
+        if b.dead == true then
+            table.remove(bullets, i)
+        end
+    end
+
+    if gameState == 2 then
+        timer = timer - dt
+        if timer <= 0 then
+            spawnZombie()
+            maxTime = 0.95 * maxTime
+            timer = maxTime
         end
     end
 end
@@ -47,11 +97,21 @@ function love.draw()
     for i,z in ipairs(zombies) do
         love.graphics.draw(sprites.zombie, z.x, z.y, zombiePlayerAngle(z), nil, nil, sprites.zombie:getWidth()/2, sprites.zombie:getHeight()/2)
     end
+
+    for i,b in ipairs(bullets) do
+        love.graphics.draw(sprites.bullet, b.x, b.y, nil, 0.42, nil, sprites.bullet:getWidth()/2, sprites.bullet:getHeight()/2)
+    end
 end
 
 function love.keypressed( key )
     if key == "space" then
         spawnZombie()
+    end
+end
+
+function love.mousepressed( x, y, button )
+    if button == 1 then
+        spawnBullet()
     end
 end
 
@@ -65,10 +125,37 @@ end
 
 function spawnZombie()
     local zombie = {}
+    zombie.x = 0
+    zombie.y = 0
+    zombie.speed = 135
+    zombie.dead = false
+    
+    local side = math.random(1, 4)
+    if side == 1 then
+        zombie.x = -30
+        zombie.y = math.random(0, love.graphics.getHeight())
+    elseif side == 2 then
+        zombie.x = love.graphics.getWidth() + 30
+        zombie.y = math.random(0, love.graphics.getHeight())
+    elseif side == 3 then
         zombie.x = math.random(0, love.graphics.getWidth())
         zombie.y = math.random(0, love.graphics.getHeight())
-        zombie.speed = 135
-        table.insert(zombies, zombie)    
+    elseif side == 4 then
+        zombie.x = math.random(0, love.graphics.getWidth())
+        zombie.y = love.graphics.getHeight() + 30
+    end
+    
+    table.insert(zombies, zombie)    
+end
+
+function spawnBullet()
+    local bullet = {}
+    bullet.x = player.x
+    bullet.y = player.y
+    bullet.speed = 450
+    bullet.dead = false
+    bullet.direction = playerMouseAngle() 
+    table.insert(bullets, bullet)
 end
 
 function distanceBetween(x1, y1, x2, y2)
